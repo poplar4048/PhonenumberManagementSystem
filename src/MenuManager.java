a89f0c5ea7a78ffd56721107e65c285d5d4dab4c
Start-Up <poplar4048@gmail.com>
2021-03-16 02:57:01 +0900

    rename x name and removed MenuManager

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
+		System.out.println("1. Add Number");
+		System.out.println("2. Delete Number");
+		System.out.println("3. Edit Number");
+		System.out.println("4. View Number");
 		System.out.println("5. Exit");
 	}
 
