92ea250c5b2ac13c13b0d7251e4cf2b1699ab904
Start-Up <poplar4048@gmail.com>
2021-03-23 01:04:04 +0900

    menu loof

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,60 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Number");
-		System.out.println("2. Delete Number");
-		System.out.println("3. Edit Number");
-		System.out.println("4. View Number");
-		System.out.println("5. Exit");
-	}
+		
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+				
+		while (num  !=6) {
+			System.out.println("1. Add Number");
+			System.out.println("2. Delete Number");
+			System.out.println("3. Edit Number");
+			System.out.println("4. View Number");
+			System.out.println("5. Show Menu");
+			System.out.println("6. Exit");
+			System.out.println("Select one number between 1-6");
+			num = input.nextInt();
+			//if (num==1) {
+				//System.out.print("Phonenumber:");
+				//int Phonenumber = input.nextInt();
+				//System.out.print("Name:");
+				//String Name = input.next();
+				//System.out.print("Phonenumber:");
+			switch(num) {
+			case 1:
+				System.out.print("Phone number:");
+				int Phonenumber = input.nextInt();
+				System.out.print("Name:");
+				String Name = input.next();
+				System.out.println("Done!");
+				break;
+			case 2:
+				System.out.print("Delete Number:");
+				String DeleteNumber = input.next();
+				System.out.println("Done!");
+				break;
+			case 3:
+				System.out.print("Before Edit Number:");
+				String BeforeEditNumber = input.next();
+				System.out.print("After Edit Number:");
+				String AfterEditNumber = input.next();
+				System.out.println("Done!");
+				break;
+			case 4:
+				System.out.println("View Number");
+				//추후에 전화번호 데이터가 등록되면 보여질곳
+				System.out.println("Done!");
+				break;
+			case 5:
+				continue;
+			case 6:
+				break;
+			
+			}
+			
+		}
 
-}
+	}
+}	
